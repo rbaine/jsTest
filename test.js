fe244b71565b0bbd561004b8dbbad1e2ac8434be
Rod <rbaine@symbience.com>
2015-05-04 21:15:22 -0700

    initialize

---
+++ test.js
@@ -0,0 +1,17 @@
+var arr = [{id:1, name:"Rod"}, {id:2, name:"Cyndy"}, {id:3, name:"Connor"}, {id:4, name:"Daxter"}];
+
+//arr.sort(function (a, b) {
+//	return a.name > b.name;
+//	});
+	
+var newArr = arr.filter( function name(element, index, array) {
+	if (element.id > 2) {
+		return element;
+	};	
+});
+	
+
+console.log(arr);
+console.log("\n");
+console.log(newArr);
+
