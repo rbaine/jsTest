456c59ed3695f9b99ee8eb601bd6c731b7bcb745
® <rbaine@symbience.com>
2015-12-07 21:36:55 -0800

    chalk

--- fs.js
+++ fs.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
+const chalk = require('chalk');
+
 
 // fs.unlink('./test.js', function (err) {
 //   if (err) throw err;
 //   console.log('successfully deleted /tmp/hello');
 // });
+
+
+console.log(chalk.blue('Hello from node with chalk.'));
 
 var buffer = [];
 
@@ -30,8 +35,16 @@ fs.readFile('./edi.x12', 'utf8', function (err, data) {
 		}
 	}
 
-	// console.log(buffer);
-	console.log( buffer[2][0] );
+	console.log(buffer);
+	console.log('\n');
+	// console.log( buffer[2][0] );
+
+
+	for (var x=0; x<buffer.length; x++) {
+		// console.log(buffer[x][0] + '\t' + buffer[x].length);
+		console.log(chalk.blue(buffer[x][0] + '\t' + buffer[x].length));
+	}
+
 });
 
 
